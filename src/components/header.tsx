afaf5f2089900eaf44ea103fa63bcf93cdb29585
Jonathan Flower <jfbloom22@gmail.com>
2024-09-24 02:42:26 -0400

    update logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="px-4 lg:px-6 h-24 flex items-center border-b border-solid border-border">
       <Link href="/" className="flex items-center justify-center" prefetch={false}>
-        <Image src="/images/logo-icon.webp" alt="AI Mastermind" width={80} height={80} className="max-h-12 w-auto" />
+        <Image src="/images/logo-wide.webp" alt="AI Mastermind" width={80} height={80} className="max-h-20 w-auto border border-gray-200 rounded-sm" />
         <span className="sr-only">AI Mastermind</span>
       </Link>
       <nav className="ml-auto flex gap-4 sm:gap-6">
